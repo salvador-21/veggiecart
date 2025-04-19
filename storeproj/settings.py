cfe2e950f41dbe2a1a00c94d3b6c64afea63b7c0
scproj21 <emkeri2190@gmail.com>
2025-04-19 11:35:33 +0800

    new update

--- storeproj/settings.py
+++ storeproj/settings.py
@@ -98,24 +98,24 @@ WSGI_APPLICATION = 'storeproj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'veggie_db',
-        'USER': 'root',
-        'PASSWORD': 'Kinsee_06',
-        'HOST': 'localhost',     # Or the IP address of your DB server
-        'PORT': '3306',          # Default MySQL port
-        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'veggie_db',
+#         'USER': 'root',
+#         'PASSWORD': 'Kinsee_06',
+#         'HOST': 'localhost',     # Or the IP address of your DB server
+#         'PORT': '3306',          # Default MySQL port
+        
+#     }
+# }
 
 
 
